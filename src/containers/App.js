895a15c84e6e94d2251a40a33ab73a8cd6327970
Marian Morcotet <marian.morcotet@fastforward.ai>
2022-07-26 21:01:34 +0300

    layout

---
+++ src/containers/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Chat from './Chat.js'
+
+const App = () => {
+  return <Chat />
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import * as ReactDOMClient from 'react-dom/client'
 import React from 'react'
-const App = () => {
-  return <h1>This is my React app!</h1>
-}
+import App from './containers/App.js'
 
 const container = document.getElementById('app')
 
 const root = ReactDOMClient.createRoot(container)
 
 root.render(<App/>);
-// console.log("TEST")
