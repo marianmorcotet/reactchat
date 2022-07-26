d6cabecb94565618c1bcf0f196c894516d20c888
Marian Morcotet <marian.morcotet@fastforward.ai>
2022-07-26 19:46:48 +0300

    base fully working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from "react-dom";
-import React from "react";
+import * as ReactDOMClient from 'react-dom/client'
+import React from 'react'
 const App = () => {
- return <h1>This is my React app!</h1>;
- }
-ReactDOM.render(<App />, document.getElementById("app"));
+  return <h1>This is my React app!</h1>
+}
+
+const container = document.getElementById('app')
+
+const root = ReactDOMClient.createRoot(container)
+
+root.render(<App/>);
+// console.log("TEST")
