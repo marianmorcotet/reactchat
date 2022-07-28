2e21949f7caa003210c755f19d55c65f82441935
Marian Morcotet <marian.morcotet@fastforward.ai>
2022-07-28 09:18:44 +0300

    cant get websocket to work

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongo = require('./mongoConnection')
+const webSocketServer = require('websocket').server
 
 const PORT = process.env.PORT || 4001
 
@@ -7,6 +8,21 @@ const app = express()
 app.get('/api/:roomId', (req, res) => {
   console.log('PARMAS', req.params)
   res.json({ message: 'Hello from server!' })
+})
+
+// app.listen(8003)
+const wsServer = new webSocketServer({
+  httpServer: app,
+  path: '/websockets',
+})
+
+wsServer.on('request', function (request) {
+  console.log(
+    new Date() +
+      ' Recieved a new connection from origin ' +
+      request.origin +
+      '.'
+  )
 })
 
 mongo.connectToDatabase()
--- src/containers/Chat/Chat.js
+++ src/containers/Chat/Chat.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Message from '../../components/Message/Message'
 import CustomInput from '../../components/CustomInput/CustomInput'
 import { useParams } from 'react-router-dom'
+import { w3cwebsocket as W3CWebSocket } from 'websocket'
 import './Chat.css'
 
 const Chat = () => {
   let { roomId } = useParams()
+  const client = new W3CWebSocket('wss://127.0.0.0:4001/websockets')
   console.log('ROOM ID', roomId)
+
+  useEffect(() => {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected')
+    }
+
+    client.onmessage = (message) => {
+      console.log(message)
+    }
+  }, [])
+
   return (
     <div className="chatRoot">
       <Message coming={false} content="message1" />
